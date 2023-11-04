5371720f958fa4baea7c509aec9042f75cdd6061
sh0311 <111671498+sh0311@users.noreply.github.com>
2023-11-04 12:27:08 +0900

    [Silver III] Title: 파도반 수열, Time: 52 ms, Memory: 31252 KB -BaekjoonHub

--- 백준/Silver/9461. 파도반 수열/파도반 수열.py
+++ 백준/Silver/9461. 파도반 수열/파도반 수열.py
@@ -1,26 +1,16 @@
 def dp(n):
-    if n==1:
-        print(1)
-    elif n==2:
-        print(1)
-    elif n==3:
-        print(1)
-    elif n==4:
-        print(2)
-    elif n==5:
-        print(2)
-    elif n>5:
-        p=[0]*(n+1)
-        p[1]=1
-        p[2] = 1
-        p[3] = 1
-        p[4] = 2
-        p[5] = 2
-        for i in range(6, n+1):
-            p[i]=p[i-1]+p[i-5]
-        print(p[n])
+
+    p[1]=1
+    p[2] = 1
+    p[3] = 1
+    p[4] = 2
+    p[5] = 2
+    for i in range(6, n+1):
+        p[i]=p[i-1]+p[i-5]
+    return(p[n])
 
 N=int(input())
+p=[0]*101
 for _ in range(N):
     n=int(input())
-    dp(n)
+    print(dp(n))
