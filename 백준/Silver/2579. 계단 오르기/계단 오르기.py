4cb0ba2b7ab8cf8072638ae2c4f1fb3531262f28
sh0311 <111671498+sh0311@users.noreply.github.com>
2023-11-12 13:47:38 +0900

    [Silver III] Title: 계단 오르기, Time: 52 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.py
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.py
@@ -1,14 +1,10 @@
 n=int(input())
 arr=[0]*301
-dp=[0]*301
-
 for i in range(1,n+1):
     arr[i]=int(input())
+dp=[0]*301
 dp[1]=arr[1]
-dp[2]=dp[1]+arr[2]
-dp[3] = max(arr[1] + arr[3], arr[2] + arr[3])
-
-
-for n in range(4,n+1):
-    dp[n]=max(dp[n-2]+arr[n],dp[n-3]+arr[n-1]+arr[n])
+dp[2]=arr[1]+arr[2]
+for i in range(3,n+1):
+    dp[i]=max(dp[i-3]+arr[i-1]+arr[i], dp[i-2]+arr[i])
 print(dp[n])
