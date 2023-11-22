cac35e0b8948b91501d32d0a4e212618bfa3c766
sh0311 <111671498+sh0311@users.noreply.github.com>
2023-11-22 14:20:51 +0900

    [Silver I] Title: 회의실 배정, Time: 280 ms, Memory: 59068 KB -BaekjoonHub

--- 백준/Silver/1931. 회의실 배정/회의실 배정.py
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.py
@@ -1,5 +1,6 @@
+import sys
 N=int(input())
-li=[list(map(int, input().split())) for _ in range(N)]
+li=[list(map(int, sys.stdin.readline().split())) for _ in range(N)]
 
 li.sort(key=lambda x:(x[1],x[0]))
 
