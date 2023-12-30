ed52769a617ee488ce88bf0b0a07e5a5267734a2
sh0311 <111671498+sh0311@users.noreply.github.com>
2023-12-30 16:57:06 +0900

    [Silver III] Title: 두 수의 합, Time: 356 ms, Memory: 28628 KB -BaekjoonHub

--- 백준/Silver/3273. 두 수의 합/두 수의 합.java
+++ 백준/Silver/3273. 두 수의 합/두 수의 합.java
@@ -1,34 +1,48 @@
-import java.util.*;
-
-
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+ 
 public class Main {
-    public static void main(String[] args) {
-        int cnt=0;
-        Scanner sc= new Scanner(System.in);
-        int n=sc.nextInt();
-        ArrayList<Integer> arr=new ArrayList<Integer>();
-        for(int i=0;i<n;i++){
-            int num=sc.nextInt();
-            arr.add(num);
-        }
-        Collections.sort(arr);
-        int x=sc.nextInt();
-        int left=0;
-        int right=arr.size()-1;
-        while (left<right){
-            int k=arr.get(left)+arr.get(right);
-            if (k<x){
-                left+=1;
-            }
-            else if (k>x){
-                right-=1;
-            }
-            else{
-                cnt+=1;
-                left+=1;
-            }
-        }
-        System.out.println(cnt);
-
-    }
+	public int count(int N, int[] arr, int X) {
+		int result = 0;
+		int sum = 0;
+		int lt = 0;
+		int rt = N-1;
+		
+		Arrays.sort(arr);
+		
+		while(lt < rt) {
+			sum = arr[rt] + arr[lt];
+			
+			if(sum == X) result++;
+			
+			if(sum < X) {
+				lt++;
+			}else {
+				rt--;
+			}
+		}
+		return result;
+	}
+	
+	public static void main(String[] args) throws IOException {
+		Main num = new Main();
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int N = Integer.valueOf(st.nextToken());
+		st = new StringTokenizer(br.readLine());
+		
+		int[] arr = new int[N];
+		
+		for(int i=0; i<N; i++) {
+			arr[i] = Integer.valueOf(st.nextToken());
+		}
+		
+		int X = Integer.valueOf(br.readLine());
+		
+		System.out.println(num.count(N, arr, X));
+	}
 }
