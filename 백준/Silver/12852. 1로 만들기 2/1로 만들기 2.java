456c2ba4f2c8e7e55f1e555c8c0de626175b8ce9
sh0311 <111671498+sh0311@users.noreply.github.com>
2024-01-01 14:39:02 +0900

    [Silver I] Title: 1로 만들기 2, Time: 224 ms, Memory: 21620 KB -BaekjoonHub

--- 백준/Silver/12852. 1로 만들기 2/1로 만들기 2.java
+++ 백준/Silver/12852. 1로 만들기 2/1로 만들기 2.java
@@ -1,74 +1,38 @@
-import java.util.*;
-
-
+import java.util.Scanner;
 
 public class Main {
-
     public static void main(String[] args) {
-       Scanner sc= new Scanner(System.in);
-       int N=sc.nextInt();
-       int [] dp=new int[N+1];
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] dp = new int[n + 1];
+        dp[0] = dp[1] = 0;
+        for(int i = 2; i<=n; i++){
+            dp[i] = dp[i-1] + 1;
+            if(i%2 == 0){
+                dp[i] = Math.min(dp[i], dp[i/2]+1);
+            }
+            if(i%3 == 0){
+                dp[i] = Math.min(dp[i], dp[i/3]+1);
+            }
+        }
+        System.out.println(dp[n]);
+        StringBuilder sb = new StringBuilder();
+        sb.append(n).append(" ");
+        while(n>1){
+            int min = dp[n-1];
+            int temp = n-1;
+            if(n%3 == 0 && dp[n/3] < min){
+                min = dp[n/3];
+                temp = n/3;
+            }
+            if(n%2 == 0 && dp[n/2] < min){
+                min = dp[n/2];
+                temp = n/2;
+            }
+            n = temp;
+            sb.append(n).append(" ");
 
-       dp[0]=dp[1]=0;
-
-       for(int i=2;i<N+1;i++) {
-           dp[i] = dp[i - 1] + 1;
-           if (i % 2 == 0)
-               dp[i] = Math.min(dp[i], dp[i / 2] + 1);
-           if (i % 3 == 0)
-               dp[i] = Math.min(dp[i], dp[i / 3] + 1);
-       }
-
-       System.out.println(dp[N]);
-
-       ArrayList<Integer> res= new ArrayList<Integer>();
-       int temp=dp[N];
-       res.add(N);
-
-
-       while(temp>0) {
-           if (N==1){
-               res.add(N);
-               break;
-           }
-           if (N % 6 == 0) {
-               if (dp[N / 3] == temp - 1) {
-                   N = N / 3;
-
-               }
-                else if (dp[N / 2] == temp - 1) {
-                   N = N / 2;
-               }else if (dp[N - 1] == temp - 1) {
-                   N = N - 1;}
-               temp = dp[N];
-               res.add(N);
-           } else if (N % 2 == 0) {
-               if (dp[N / 2] == temp - 1) {
-                   N = N / 2;
-               } else if (dp[N - 1] == temp - 1) {
-                   N = N - 1;
-               }
-               temp = dp[N];
-               res.add(N);
-           } else if (N % 3 == 0) {
-               if (dp[N / 3] == temp - 1) {
-                   N = N / 3;
-               } else if (dp[N - 1] == temp - 1) {
-                   N = N - 1;
-               }
-
-               temp = dp[N];
-               res.add(N);
-           } else {
-               N = N - 1;
-               temp = dp[N];
-               res.add(N);
-           }
-       }
-
-       for( int r : res){
-           System.out.print(r+ " ");
-       }
-
+        }
+        System.out.print(sb.toString().trim());
     }
 }
