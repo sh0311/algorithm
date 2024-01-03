35f90ec8be213c0d5a8ee3198441333845ed0410
sh0311 <111671498+sh0311@users.noreply.github.com>
2024-01-03 13:33:17 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 4308 ms, Memory: 50552 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.py
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.py
@@ -6,23 +6,20 @@ for _ in range(N-1):
     graph[a].append(b)
     graph[b].append(a)
 
-parent=[None for _ in range(N+1)]
-visited=[0 for _ in range(N+1)]
+parent=[0 for _ in range(N+1)]
+visited=[False]*(N+1)
 
 def BFS(visited, parent, n):
     deq=deque([n])
-    visited[n]=1
+    visited[n]=True
     while deq:
         v = deq.popleft()
         for g in graph[v]:
-            if visited[g] == 0:
+            if not visited[g]:
                 deq.append(g)
-                visited[g]=1
+                visited[g]=True
                 parent[g]=v
 
 BFS(visited, parent, 1)
 for i in range(2,N+1):
     print(parent[i])
-
-
-
