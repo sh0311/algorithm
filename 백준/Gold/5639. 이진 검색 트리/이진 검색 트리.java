0b998a32453758f07053d275058784fff77bd1ad
sh0311 <111671498+sh0311@users.noreply.github.com>
2024-01-09 12:42:07 +0900

    [Gold V] Title: 이진 검색 트리, Time: 668 ms, Memory: 19284 KB -BaekjoonHub

--- 백준/Gold/5639. 이진 검색 트리/이진 검색 트리.java
+++ 백준/Gold/5639. 이진 검색 트리/이진 검색 트리.java
@@ -4,33 +4,60 @@ import java.util.*;
 
 
 public class Main {
-    static ArrayList<Integer> tree=new ArrayList<>();
-    static void post(int start, int end){
-        if (start>end) return;
-        int mid=end+1;
-        for(int i=start+1;i<end+1;i++){
-            if (tree.get(i)>tree.get(start)){
-                mid=i;
-                break;
-            }
+    static class Node{
+        int num;
+        Node right, left;
+
+        Node(int n){
+            this.num=n;
         }
-        post(start+1,mid-1);
-        post(mid,end);
-        System.out.println(tree.get(start));
+
+        void insert(int num){
+            if(num>this.num){
+                if (this.right==null){
+                    Node inp=new Node(num);
+                    this.right=inp;
+                }
+                else{
+                    this.right.insert(num);
+                }
+            }
+            else if(num<this.num){
+                if (this.left==null){
+                    Node inp=new Node(num);
+                    this.left=inp;
+                }
+                else{
+                    this.left.insert(num);
+                }
+            }
+
+
+        }
+
     }
 
+    static void post(Node root){
+        if (root.left!=null)
+            post(root.left);
+        if (root.right!=null)
+            post(root.right);
+        System.out.println(root.num);
+    }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Node root=new Node(Integer.parseInt(br.readLine()));
 
         String input;
         while(true){
             input=br.readLine();
             if(input==null||input.equals(""))
                 break;
-            tree.add(Integer.parseInt(input));
+            int inpu=Integer.parseInt(input);
+            root.insert(inpu);
         }
-        post(0,tree.size()-1);
+        post(root);
 
 
     }
