db003fe8c6c36f099e4014fec8193cf0742b457d
sh0311 <111671498+sh0311@users.noreply.github.com>
2024-05-12 11:29:18 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 1756 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.cc
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.cc
@@ -1,34 +1,43 @@
 #include <iostream>
-#include <algorithm>
 #include <vector>
-#include <map>
+#include <algorithm>
+#include <stack>
+#include <queue>
 
 using namespace std;
 
+void quickSort(vector<int> &v, int k, int start, int end);
+void partition(vector<int> &v, int start, int end);
+void swap(vector<int> &v,int i, int j);
 
+static vector<int> v;
+static vector<int> temp;
+void merge(int start, int end);
 
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
+    ios::sync_with_stdio(false);
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
 
     int n;
     cin>>n;
 
-    int count[10001]={0}; //배열 초기화하는 방법
+    int count[10001]={0};
 
     for(int i=0;i<n;i++){
         int num;
         cin>>num;
         count[num]+=1;
-
     }
-
-    for(int i=1;i<10001;i++) {
-        for (int j = 0; j < count[i]; j++)
-            cout << i << '\n';
-        
+    for(int i=0;i<10001;i++){
+        if(count[i]!=0){
+           for(int j=0;j<count[i];j++){
+               cout<<i<<'\n';
+           }
+        }
     }
-
-
     return 0;
+
+
+
 }
