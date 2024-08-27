5428a9b66bf916b6515cb777b4b6d2ded4913627
sh0311 <111671498+sh0311@users.noreply.github.com>
2024-08-27 15:57:23 +0900

    [Gold IV] Title: 오큰수, Time: 200 ms, Memory: 14120 KB -BaekjoonHub

--- 백준/Gold/17298. 오큰수/오큰수.cc
+++ 백준/Gold/17298. 오큰수/오큰수.cc
@@ -1,13 +1,21 @@
 #include <iostream>
-#include <vector>
 #include <algorithm>
 #include <stack>
-#include <queue>
+#include <vector>
 
 using namespace std;
 
+void cal(int n, stack<int>& st,vector<int>& vec, vector<int>& origin){
+    for(int i=0;i<n;i++){
+        while(!st.empty()&&origin[st.top()]<origin[i]){
+            vec[st.top()]=origin[i];
+            st.pop();
+        }
+        st.push(i);
+    }
+}
 
-int main(){
+int main() {
     ios::sync_with_stdio(false);
     ios_base::sync_with_stdio(0);
     cin.tie(0);
@@ -15,33 +23,23 @@ int main(){
     int n;
     cin>>n;
 
-    int A[n];
-    for(int i=0;i<n;i++){
-        cin>>A[i];
-    }
+    stack<int> st; //인덱스 저장
+    vector<int>vec(n,-1);  //결과값 담는 벡터
+    vector<int> origin(n); //초기값들 담는 벡터
 
-    stack<int> st;
 
-    int result[n];
 
     for(int i=0;i<n;i++){
-        while(!st.empty()&&A[i]>A[st.top()]){
-            result[st.top()]=A[i];
-            st.pop();
-        }
-        st.push(i);
+        cin>>origin[i];
     }
-
-    while(!st.empty()){
-        result[st.top()]=-1;
-        st.pop();
-    }
+    cal(n, st, vec, origin);
 
     for(int i=0;i<n;i++){
-        cout<<result[i]<<" ";
+        cout<<vec[i]<<" ";
     }
 
-
-    return 0;
 }
+
+
+
 
