1234479c6d75aca33b9254c0cde316f0acce67a5
Seunghyun Nam <111671498+sh0311@users.noreply.github.com>
2025-01-25 22:56:24 +0900

    [Gold V] Title: 로봇 청소기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/14503. 로봇 청소기/로봇 청소기.cc
+++ 백준/Gold/14503. 로봇 청소기/로봇 청소기.cc
@@ -1,86 +1,74 @@
-#include <iostream>
-#include <vector>
+/******************************************************************************
 
+Welcome to GDB Online.
+  GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+  C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, COBOL, HTML, CSS, JS
+  Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+#include <vector>
+#include <iostream>
 
 using namespace std;
-bool visited[50][50]={false};
-int dx[4]={0, 1, 0, -1};  // 북 동 남 서 순서
-int dy[4]={-1, 0, 1, 0};
 
-bool around(int x, int y, int N, int M, vector<vector<int> > &raod) {
-    for(int i=0; i<4; i++) {
-        int new_x = x + dx[i];
-        int new_y = y + dy[i];
-        if(new_x>= 0 && new_x < M && new_y >= 0 && new_y < N &&
-            !visited[new_y][new_x] && raod[new_y][new_x] == 0) {
-            return true;
-            }
+int dx[4]={0,1,0,-1}; //북, 서, 남, 동
+int dy[4]={-1,0,1,0};
+vector<vector<int>> graph;
+int cnt=0;
+int n,m;
+
+void sol(int r,int c, int d){
+    int now=graph[r][c];
+    if(now==0){
+        graph[r][c]=2;
+        cnt++;
     }
-    return false;
+    
+    //청소되지 않은 칸 있는 경우
+    for(int i=0;i<4;i++){
+        d=(d+3)%4;
+        int nextX=c+dx[d];
+        int nextY=r+dy[d];
+        if(graph[nextY][nextX]==0){
+            r=nextY;
+            c=nextX;
+            sol(r,c,d);
+            return;
+        }
+    }
+    
+    //청소되지 않은 칸 없는 경우
+    int back=graph[r+dy[(d+2)%4]][c+dx[(d+2)%4]];
+    if(back==1||r+dy[(d+2)%4]<0||r+dy[(d+2)%4]>=n||c+dx[(d+2)%4]<0||c+dx[(d+2)%4]>=m){
+        return;
+    } 
+    r=r+dy[(d+2)%4];
+    c=c+dx[(d+2)%4];
+    sol(r,c,d);
+    return;
 }
 
 
-int sol(vector<vector<int>>& road, int r, int c, int d, int N, int M) {
-    visited[r][c]=true;
-    int cnt=1;
 
-    while(true) {
-        if (around(c, r, N, M, road)) {
-            //다음 방향(반시계)
-            for(int i=0; i<4; i++) {
-                d=(d+3)%4;
-                int new_x=c+dx[d];
-                int new_y=r+dy[d];
 
-                if(new_x>= 0 && new_x < M && new_y >= 0 && new_y < N &&
-                    !visited[new_y][new_x] && road[new_y][new_x]==0) {
-
-                    visited[new_y][new_x]=true;
-                    c=new_x;
-                    r=new_y;
-                    cnt++;
-                    break;
-                    }
-            }
-        }
-        //주위에 빈 칸 없으면
-        else {
-            int back_d=(d+2)%4;
-            int new_x=c+dx[back_d];
-            int new_y=r+dy[back_d];
-            if(new_x < 0 || new_x >= M || new_y < 0 || new_y >= N ||
-                road[new_y][new_x]==1) {
-                break;
-                }
-            c=new_x;
-            r=new_y;
-        }
-    }
-
-    return cnt;
-}
-
-int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(0);
-
-    int N,M;
-    cin>>N>>M;
-
+int main()
+{
+    cin>>n>>m;
     int r,c,d;
     cin>>r>>c>>d;
-
-    vector<vector<int>> road(N,vector<int>(M));
-
-    for(int i=0;i<N;i++)
-        for(int j=0;j<M;j++) {
-            cin>>road[i][j];
+    
+    graph.resize(n,vector<int>(m,-1));
+    
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            cin>>graph[i][j];
         }
+    }
+    
+    sol(r,c,d);
 
-
-    int res=sol(road, r, c, d, N, M);
-    cout<<res;
-
+    
+    cout<<cnt;
     return 0;
-
 }
